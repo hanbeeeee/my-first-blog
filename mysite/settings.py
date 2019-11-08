b618c14cb99d51deaa55bf0137d483ac1d4c3877
hanbee <t_op1104@naver.com>
2019-11-08 11:04:24 +0900

    changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -117,6 +117,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+AUTH_USER_MODEL = ''
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
